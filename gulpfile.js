aaae2499dd5fe41269cee56b128f93b2d482d4d8
Trivernis <trivernis@gmail.com>
2019-08-21 14:16:47 +0200

    Initial structure

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const {src, dest, watch, series, task} = require('gulp');
+const sass = require('gulp-sass');
+const ts = require('gulp-typescript');
+const minify = require('gulp-minify');
+const del = require('delete');
+
+
+function clearDist(cb) {
+    del('dist/*', cb);
+}
+
+function compileTypescript() {
+    let tsProject = ts.createProject('tsconfig.json');
+    let tsResult = tsProject.src().pipe(tsProject());
+    return tsResult
+    //.pipe(minify())
+        .pipe(dest('dist'));
+}
+
+function minifyJs() {
+    return src('src/public/javascripts/**/*.js')
+        .pipe(minify({
+            ext: {
+                src: '-debug.js',
+                min: '.js'
+            }
+        }))
+        .pipe(dest('dist/public/javascripts'));
+}
+
+function compileSass() {
+    return src('src/public/stylesheets/sass/**/style.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('dist/public/stylesheets'));
+}
+
+task('default', series(clearDist, compileTypescript, minifyJs, compileSass));
+task('watch', () => {
+    watch('src/public/stylesheets/sass/**/*.sass', compileSass);
+    watch('**/*.js', minifyJs);
+    watch('**/*.ts', compileTypescript);
+});
