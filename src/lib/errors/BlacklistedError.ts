54646e6b73dbb1e2b7b73c99f23de083259760d7
trivernis <trivernis@gmail.com>
2020-01-18 17:39:54 +0100

    Add space to BlacklistedError phrase list

--- src/lib/errors/BlacklistedError.ts
+++ src/lib/errors/BlacklistedError.ts
@@ -5,6 +5,6 @@ import {BaseError} from "./BaseError";
  */
 export class BlacklistedError extends BaseError {
     constructor(public phrases: string[], field: string = "input") {
-        super(`The ${field} contains the blacklisted words: ${phrases.join(",")}`);
+        super(`The ${field} contains the blacklisted words: ${phrases.join(", ")}`);
     }
 }
